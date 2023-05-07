93ee357ac4f2337559485783f668dec47f1bea4e
Filip Varda <filip@oats.tech>
2023-05-07 21:27:52 +0200

    UI update

--- Sources/Revlum/Extensions/UIColor+Extension.swift
+++ Sources/Revlum/Extensions/UIColor+Extension.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 extension UIColor {
-    static let primaryColor = UIColor(named: "primaryColor")!
-    static let secondaryColor = UIColor(named: "secondaryColor")!
-    static let textMainColor = UIColor(named: "textMainColor")!
-    static let textDescriptionColor = UIColor(named: "textDescriptionColor")!
-    static let whiteColor = UIColor(named: "bgColor")!
+    static let primaryColor: UIColor = {
+        let bundle = Bundle.module
+        let color = UIColor(named: "primaryColor", in: bundle, compatibleWith: nil)!
+        return color
+    }()
+    static let secondaryColor: UIColor = {
+        let bundle = Bundle.module
+        let color = UIColor(named: "secondaryColor", in: bundle, compatibleWith: nil)!
+        return color
+    }()
+    static let textMainColor: UIColor = {
+        let bundle = Bundle.module
+        let color = UIColor(named: "textMainColor", in: bundle, compatibleWith: nil)!
+        return color
+    }()
+    static let textDescriptionColor: UIColor = {
+        let bundle = Bundle.module
+        let color = UIColor(named: "textDescriptionColor", in: bundle, compatibleWith: nil)!
+        return color
+    }()
+    static let bgColor: UIColor = {
+        let bundle = Bundle.module
+        let color = UIColor(named: "bgColor", in: bundle, compatibleWith: nil)!
+        return color
+    }()
 }
