2c85e8c34e875f900d1a43925505bd41ee8a3b14
Filip Varda <filip@oats.tech>
2023-05-07 21:28:45 +0200

    UI update

--- Sources/Revlum/Views/RevlumSegmentedControl.swift
+++ Sources/Revlum/Views/RevlumSegmentedControl.swift
@@ -27,7 +27,7 @@ class RevlumSegmentedControl: UISegmentedControl {
 
     private func setupControl() {
         selectedSegmentIndex = 0
-        backgroundColor = .whiteColor
+        backgroundColor = .bgColor
         setTitleTextAttributes([.foregroundColor: UIColor.textMainColor], for: .normal)
         setTitleTextAttributes([.foregroundColor: UIColor.textMainColor,
                                 .underlineStyle: NSUnderlineStyle.single.rawValue,
