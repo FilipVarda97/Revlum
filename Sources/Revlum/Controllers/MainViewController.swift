66c3e1aff6d6698c1f18cade0057bf4037d8292f
Filip Varda <filip@oats.tech>
2023-05-07 19:07:40 +0200

    UI update

--- Sources/Revlum/Controllers/MainViewController.swift
+++ Sources/Revlum/Controllers/MainViewController.swift
@@ -70,6 +70,8 @@ public final class MainViewController: UIViewController {
         segmentedControl.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
         dissmissButton.addTarget(self, action: #selector(dismissPressed), for: .touchUpInside)
 
+        brandBackgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        brandLogoImageView.translatesAutoresizingMaskIntoConstraints = false
         dissmissButton.translatesAutoresizingMaskIntoConstraints = false
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         containerView.translatesAutoresizingMaskIntoConstraints = false
