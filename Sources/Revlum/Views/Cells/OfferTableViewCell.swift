77c1a95f145bfe107f66e3dde55a5214d07e10da
Filip Varda <filip@oats.tech>
2023-05-09 16:32:00 +0200

    UI update

--- Sources/Revlum/Views/Cells/OfferTableViewCell.swift
+++ Sources/Revlum/Views/Cells/OfferTableViewCell.swift
@@ -38,13 +38,16 @@ final class OfferTableViewCell: UITableViewCell {
     private func setUpViews() {
         contentView.layer.cornerRadius = 5
         contentView.clipsToBounds = true
-        contentView.backgroundColor = .bgColor
+        contentView.backgroundColor = .clear
+        containerView.backgroundColor = .bgColor
 
         titleLabel.textColor = .textMainColor
         descriptionLabel.textColor = .textDescriptionColor
         offerButton.layer.cornerRadius = 10
         offerButton.backgroundColor = .primaryColor
         offerButton.setTitleColor(.white, for: .normal)
+        offerImageView.layer.cornerRadius = 5
+        offerImageView.clipsToBounds = true
 
         contentView.addSubview(containerView)
         containerView.addSubviews(offerImageView,
@@ -73,11 +76,13 @@ final class OfferTableViewCell: UITableViewCell {
         ])
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: offerImageView.topAnchor),
-            titleLabel.leftAnchor.constraint(equalTo: offerImageView.rightAnchor, constant: 12)
+            titleLabel.leftAnchor.constraint(equalTo: offerImageView.rightAnchor, constant: 12),
+            titleLabel.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -10)
         ])
         NSLayoutConstraint.activate([
-            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 6),
-            descriptionLabel.leftAnchor.constraint(equalTo: titleLabel.leftAnchor)
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -6),
+            descriptionLabel.leftAnchor.constraint(equalTo: titleLabel.leftAnchor),
+            descriptionLabel.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -10)
         ])
         NSLayoutConstraint.activate([
             offerButton.bottomAnchor.constraint(equalTo: offerImageView.bottomAnchor),
