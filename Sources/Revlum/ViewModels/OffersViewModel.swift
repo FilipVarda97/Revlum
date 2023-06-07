57891ef3b6c81d498a69fc2f1af3b90870cda718
Filip Varda <filip@oats.tech>
2023-06-08 00:53:53 +0200

    UI updates

--- Sources/Revlum/ViewModels/OffersViewModel.swift
+++ Sources/Revlum/ViewModels/OffersViewModel.swift
@@ -92,7 +92,7 @@ extension OffersViewModel: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 153
+        return 169
     }
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
