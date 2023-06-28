5651f8a1fcfadc2ec1df5ab955c4b9b3a88bb710
Filip Varda <filip@oats.tech>
2023-06-28 23:05:38 +0200

    UI updates

--- Sources/Revlum/Views/FilterView/Items/RevlumDeviceFilterItem.swift
+++ Sources/Revlum/Views/FilterView/Items/RevlumDeviceFilterItem.swift
@@ -23,6 +23,7 @@ class RevlumDeviceFilterItem: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         selectionStyle = .default
+        backgroundColor = .clear
         setUpViews()
         setUpConstraints()
     }
